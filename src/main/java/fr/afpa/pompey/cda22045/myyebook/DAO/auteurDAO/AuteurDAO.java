5202921ed0aa1385cdce54f9d09004b8608e480b
mevine54 <mevinecascadeur@gmail.com>
2025-01-21 12:35:27 +0100

    dao_1 ok

--- src/main/java/fr/afpa/pompey/cda22045/myyebook/DAO/auteurDAO/AuteurDAO.java
+++
@@ -1,7 +0,0 @@
-package fr.afpa.pompey.cda22045.myyebook.DAO.auteurDAO;
-
-import fr.afpa.pompey.cda22045.myyebook.DAO.DAO;
-import fr.afpa.pompey.cda22045.myyebook.model.Auteur;
-
-public interface AuteurDAO extends DAO<Auteur> {
-}
--- src/main/java/fr/afpa/pompey/cda22045/myyebook/DAO/compteDAO/CompteDAO.java
+++
@@ -1,7 +0,0 @@
-package fr.afpa.pompey.cda22045.myyebook.DAO.compteDAO;
-
-import fr.afpa.pompey.cda22045.myyebook.DAO.DAO;
-import fr.afpa.pompey.cda22045.myyebook.model.Compte;
-
-public interface CompteDAO extends DAO<Compte> {
-}
