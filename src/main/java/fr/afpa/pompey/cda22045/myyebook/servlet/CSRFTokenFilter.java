2e0df80b034810b89d5d9c7ad0b602d79b1ce949
mevine54 <mevinecascadeur@gmail.com>
2025-01-24 11:47:08 +0100

    CSRFTokenFilter ok

--- src/main/java/fr/afpa/pompey/cda22045/myyebook/servlet/CSRFTokenFilter.java
+++ src/main/java/fr/afpa/pompey/cda22045/myyebook/servlet/CSRFTokenFilter.java
@@ -2,23 +2,43 @@ package fr.afpa.pompey.cda22045.myyebook.servlet;
 
 import fr.afpa.pompey.cda22045.myyebook.securite.CSRFTokenUtil;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-
+@WebFilter("*")
 public class CSRFTokenFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpSession session = httpRequest.getSession();
 
-        if (session!=null && session.getAttribute("csrfToken") == null) {
-            String csrfToken = CSRFTokenUtil.generateCSRFToken();
-            session.setAttribute("csrfToken", csrfToken);
+        // Convertir en HttpServletRequest/HttpServletResponse
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        System.out.println("filtre csrf appele");
+        HttpSession session = httpRequest.getSession(true);
+
+        // Vérifier uniquement pour les requêtes sensibles (POST, PUT, DELETE)
+        String method = httpRequest.getMethod();
+        if ( session!=null && (method.equalsIgnoreCase("POST") || method.equalsIgnoreCase("PUT") || method.equalsIgnoreCase("DELETE"))) {
+            // Récupérer le token CSRF envoyé par le client
+            String csrfTokenFromClient = httpRequest.getParameter("csrfToken");
+
+            // Récupérer le token CSRF stocké dans la session
+            String csrfTokenFromServer = (String) session.getAttribute("csrfToken");
+            System.out.println("filtre csrf applique");
+
+            // Validation
+            if (csrfTokenFromClient == null || !csrfTokenFromClient.equals(csrfTokenFromServer)) {
+                // Rejet si le token est invalide ou absent
+                System.out.println("filtre csrf valide");
+
+                httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid CSRF token");
+            }
         }
         chain.doFilter(request, response);
+
     }
 }
-
-
