d1b98c469b781db798262584672e84f634915fca
youcefmei <youcefmei@aol.com>
2025-02-11 06:46:45 +0100

    feat: simplification bdd

--- script_bdd/MYYEBOOK_SCRIPT.sql
+++ script_bdd/MYYEBOOK_SCRIPT.sql
@@ -1,98 +1,232 @@
 
+
 DROP DATABASE IF EXISTS myyebook;
 CREATE DATABASE myyebook;
 USE myyebook;
 
 CREATE TABLE Auteur(
-                       aut_id INT AUTO_INCREMENT,
-                       aut_nom VARCHAR(50) NOT NULL,
-                       aut_prenom VARCHAR(50),
-                       aut_photo VARCHAR(250) NOT NULL,
-                       PRIMARY KEY(aut_id)
+   aut_id INT AUTO_INCREMENT,
+   aut_nom VARCHAR(50) NOT NULL,
+   aut_prenom VARCHAR(50),
+   aut_photo VARCHAR(250) NOT NULL,
+   CONSTRAINT PK_Auteur PRIMARY KEY(aut_id)
 );
 
 CREATE TABLE Compte(
-                       cpt_id INT AUTO_INCREMENT,
-                       cpt_login VARCHAR(20) NOT NULL,
-                       cpt_mdp VARCHAR(300) NOT NULL,
-                       cpt_date DATE NOT NULL DEFAULT (CURRENT_DATE) ,
-                       cpt_role ENUM( 'ROLE_CLIENT','ROLE_LIBRAIRE','ROLE_LIBRAIRE_ATTENTE') NOT NULL ,
-                       PRIMARY KEY(cpt_id),
-                       UNIQUE(cpt_login)
+   cpt_id INT AUTO_INCREMENT,
+   cpt_login VARCHAR(20) NOT NULL,
+   cpt_mdp VARCHAR(300) NOT NULL,
+   cpt_role ENUM( 'ROLE_CLIENT','ROLE_LIBRAIRE','ROLE_LIBRAIRE_ATTENTE') NOT NULL,
+   cpt_date DATE NOT NULL  DEFAULT (CURRENT_DATE) ,
+   CONSTRAINT PK_Compte PRIMARY KEY(cpt_id),
+   CONSTRAINT AK_Compte UNIQUE(cpt_login)
 );
 
 CREATE TABLE Categorie(
-                          cat_id INT AUTO_INCREMENT,
-                          cat_nom VARCHAR(100) NOT NULL,
-                          PRIMARY KEY(cat_id),
-                          UNIQUE(cat_nom)
+   cat_id INT AUTO_INCREMENT,
+   cat_nom VARCHAR(100) NOT NULL,
+   CONSTRAINT PK_Categorie PRIMARY KEY(cat_id),
+   CONSTRAINT AK_Categorie UNIQUE(cat_nom)
 );
 
 CREATE TABLE Client(
-                       cli_id INT AUTO_INCREMENT,
-                       cli_nom VARCHAR(50) NOT NULL,
-                       cli_prenom VARCHAR(50) NOT NULL,
-                       cli_email VARCHAR(50) NOT NULL,
-                       cli_adresse VARCHAR(250) NOT NULL,
-                       cli_ville VARCHAR(50) NOT NULL,
-                       cli_code_postale VARCHAR(8),
-                       cpt_id INT NOT NULL,
-                       PRIMARY KEY(cli_id),
-                       UNIQUE(cpt_id),
-                       FOREIGN KEY(cpt_id) REFERENCES Compte(cpt_id)
+   cli_id INT AUTO_INCREMENT,
+   cli_nom VARCHAR(50) NOT NULL,
+   cli_prenom VARCHAR(50) NOT NULL,
+   cli_email VARCHAR(50) NOT NULL,
+   cli_adresse VARCHAR(250) NOT NULL,
+   cli_ville VARCHAR(50) NOT NULL,
+   cli_code_postale VARCHAR(8),
+   cpt_id INT NOT NULL,
+   CONSTRAINT PK_Client PRIMARY KEY(cli_id),
+   CONSTRAINT AK_Client UNIQUE(cpt_id),
+   CONSTRAINT FK_Client_Compte FOREIGN KEY(cpt_id) REFERENCES Compte(cpt_id)
 );
 
 CREATE TABLE Libraire(
-                         lib_id INT AUTO_INCREMENT,
-                         lib_nom VARCHAR(50) NOT NULL,
-                         lib_prenom VARCHAR(50) NOT NULL,
-                         lib_est_approuve BOOLEAN ,
-                         cpt_id INT NOT NULL,
-                         PRIMARY KEY(lib_id),
-                         UNIQUE(cpt_id),
-                         FOREIGN KEY(cpt_id) REFERENCES Compte(cpt_id)
+   lib_id INT AUTO_INCREMENT,
+   lib_nom VARCHAR(50) NOT NULL,
+   lib_prenom VARCHAR(50) NOT NULL,
+   lib_est_approuve BOOLEAN NOT NULL,
+   cpt_id INT NOT NULL,
+   CONSTRAINT PK_Libraire PRIMARY KEY(lib_id),
+   CONSTRAINT AK_Libraire UNIQUE(cpt_id),
+   CONSTRAINT FK_Libraire_Compte FOREIGN KEY(cpt_id) REFERENCES Compte(cpt_id)
 );
 
 CREATE TABLE Livre(
-                      liv_id INT AUTO_INCREMENT,
-                      liv_titre VARCHAR(100) NOT NULL,
-                      liv_resume VARCHAR(2000) NOT NULL,
-                      liv_photo VARCHAR(250) NOT NULL,
-                      liv_en_avant BOOLEAN NOT NULL DEFAULT false,
-                      aut_id INT NOT NULL,
-                      cat_id INT NOT NULL,
-                      PRIMARY KEY(liv_id),
-                      FOREIGN KEY(aut_id) REFERENCES Auteur(aut_id),
-                      FOREIGN KEY(cat_id) REFERENCES Categorie(cat_id)
-);
-
-CREATE TABLE Exemplaire(
-                           exe_id INT AUTO_INCREMENT,
-                           liv_id INT NOT NULL,
-                           PRIMARY KEY(exe_id),
-                           FOREIGN KEY(liv_id) REFERENCES Livre(liv_id)
-);
-
-CREATE TABLE Reservation(
-                            res_id INT AUTO_INCREMENT,
-                            res_date DATETIME,
-                            liv_id INT NOT NULL,
-                            cli_id INT NOT NULL,
-                            PRIMARY KEY(res_id),
-                            FOREIGN KEY(liv_id) REFERENCES Livre(liv_id),
-                            FOREIGN KEY(cli_id) REFERENCES Client(cli_id)
+   liv_id INT AUTO_INCREMENT,
+   liv_titre VARCHAR(100) NOT NULL,
+   liv_resume VARCHAR(2000) NOT NULL,
+   liv_photo VARCHAR(250) NOT NULL,
+   liv_en_avant BOOLEAN NOT NULL DEFAULT false,
+   quantite INT NOT NULL,
+   aut_id INT NOT NULL,
+   cat_id INT NOT NULL,
+   CONSTRAINT PK_Livre PRIMARY KEY(liv_id),
+   CONSTRAINT FK_Livre_Auteur FOREIGN KEY(aut_id) REFERENCES Auteur(aut_id),
+   CONSTRAINT FK_Livre_Categorie FOREIGN KEY(cat_id) REFERENCES Categorie(cat_id)
 );
 
 CREATE TABLE Emprunter(
-                          emp_id INT AUTO_INCREMENT PRIMARY KEY,
-                          res_id INT,
-                          emp_date_emprunt DATETIME NOT NULL,
-                          emp_date_retour DATETIME,
-                          cli_id INT NOT NULL,
-                          lib_id INT NOT NULL,
-                          exe_id INT NOT NULL,
-                          FOREIGN KEY(res_id) REFERENCES Reservation(res_id),
-                          FOREIGN KEY(cli_id) REFERENCES Client(cli_id),
-                          FOREIGN KEY(lib_id) REFERENCES Libraire(lib_id),
-                          FOREIGN KEY(exe_id) REFERENCES Exemplaire(exe_id)
+   cli_id INT,
+   lib_id INT,
+   liv_id INT,
+   emp_res_date DATETIME NOT NULL,
+   emp_date_emprunt DATETIME,
+   emp_date_retour DATETIME,
+   CONSTRAINT PK_Emprunter PRIMARY KEY(cli_id, lib_id, liv_id),
+   CONSTRAINT FK_Emprunter_Client FOREIGN KEY(cli_id) REFERENCES Client(cli_id),
+   CONSTRAINT FK_Emprunter_Libraire FOREIGN KEY(lib_id) REFERENCES Libraire(lib_id),
+   CONSTRAINT FK_Emprunter_Livre FOREIGN KEY(liv_id) REFERENCES Livre(liv_id)
 );
+
+DELIMITER $$;
+CREATE TRIGGER validate_compte
+BEFORE INSERT ON Client
+FOR EACH ROW
+BEGIN
+    DECLARE count_libraire INT;
+    DECLARE count_client INT;
+
+    -- Vérifie si le compte est déjà utilisé par un libraire
+    SELECT
+        COUNT(*) INTO count_libraire
+    FROM
+        Libraire
+    WHERE
+        cpt_id = NEW.cpt_id;
+
+    -- Si le compte est utilisé par un libraire, empêche l'insertion
+    IF count_libraire > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Ce compte est déjà utilisé par un libraire.';
+    END IF;
+
+    -- Vérifie si le compte est déjà utilisé par un client
+    SELECT
+        COUNT(*) INTO count_client
+    FROM
+        Client
+    WHERE
+        cpt_id = NEW.cpt_id;
+
+    -- Si le compte est utilisé par un client, empêche l'insertion
+    IF count_client > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Ce compte est déjà utilisé par un client.';
+    END IF;
+END $$
+DELIMITER ;
+
+DELIMITER $$;
+CREATE TRIGGER validate_libraire
+BEFORE INSERT ON Libraire
+FOR EACH ROW
+BEGIN
+    DECLARE count_libraire INT;
+    DECLARE count_client INT;
+    -- Vérifie si le compte est déjà utilisé par un client
+    SELECT
+        COUNT(*) INTO count_client
+    FROM
+        Client
+    WHERE
+        cpt_id = NEW.cpt_id;
+    -- Si le compte est utilisé par un client, empêche l'insertion
+    IF count_client > 0 THEN SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Ce compte est déjà utilisé par un client.';
+    END IF;
+    -- Vérifie si le compte est déjà utilisé par un libraire
+    SELECT
+        COUNT(*) INTO count_libraire
+    FROM
+        Libraire
+    WHERE
+        cpt_id = NEW.cpt_id;
+    -- Si le compte est utilisé par un libraire, empêche l'insertion
+    IF
+        count_libraire > 0
+    THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ce compte est déjà utilisé par un libraire.';
+    END IF;
+END $$
+DELIMITER ;
+
+
+DROP table Emprunter;
+
+CREATE TABLE Emprunter(
+   emp_id INT AUTO_INCREMENT PRIMARY KEY,
+   emp_res_date DATETIME NOT NULL,
+   emp_date_emprunt DATETIME ,
+   emp_date_retour DATETIME,
+   cli_id INT NOT NULL,
+   lib_id INT NOT NULL,
+   liv_id INT NOT NULL,
+   FOREIGN KEY(cli_id) REFERENCES Client(cli_id),
+   FOREIGN KEY(lib_id) REFERENCES Libraire(lib_id),
+   FOREIGN KEY(liv_id) REFERENCES Livre(liv_id)
+);
+
+DELIMITER //
+
+-- Trigger pour vérifier que la date d'emprunt est après la date de réservation
+CREATE TRIGGER before_insert_emprunt
+BEFORE INSERT ON Emprunter
+FOR EACH ROW
+BEGIN
+    IF NEW.emp_date_emprunt IS NOT NULL AND NEW.emp_date_emprunt <= NEW.emp_res_date THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La date d\'emprunt doit être après la date de réservation.';
+    END IF;
+END;
+//
+
+-- Trigger pour vérifier que la date de retour est après la date d'emprunt
+CREATE TRIGGER before_insert_retour
+BEFORE INSERT ON Emprunter
+FOR EACH ROW
+BEGIN
+    IF NEW.emp_date_retour IS NOT NULL AND NEW.emp_date_retour <= NEW.emp_date_emprunt THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La date de retour doit être après la date d\'emprunt.';
+    END IF;
+END;
+//
+
+-- Trigger pour vérifier que la date de retour ne peut être fixée que si la date d'emprunt est fixée
+CREATE TRIGGER before_update_retour
+BEFORE UPDATE ON Emprunter
+FOR EACH ROW
+BEGIN
+    IF NEW.emp_date_retour IS NOT NULL AND NEW.emp_date_emprunt IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La date de retour ne peut être fixée que si la date d\'emprunt est fixée.';
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE TRIGGER before_update_emprunt
+BEFORE UPDATE ON Emprunter
+FOR EACH ROW
+BEGIN
+    DECLARE livre_quantite INT;
+
+    -- Récupérer la quantité du livre
+    SELECT quantite INTO livre_quantite
+    FROM Livre
+    WHERE liv_id = NEW.liv_id;
+
+    -- Vérifier si la quantité est zéro et si la date d'emprunt est fixée
+    IF livre_quantite = 0 AND NEW.emp_date_emprunt IS NOT NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La date d\'emprunt ne peut pas être fixée car la quantité du livre est zéro.';
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+ALTER TABLE Libraire
+ADD COLUMN quantite INT NOT NULL DEFAULT 0,
+ADD CONSTRAINT CHK_Quantite CHECK (quantite >= 0);
--- script_bdd/insertMyyeBook.sql
+++ script_bdd/insertMyyeBook.sql
@@ -17,7 +17,6 @@ VALUES ('Hugo', 'Victor', '4313ade0-c1f5-45fc-9507-208b314cadcb.jpg'),
        ('bnvnvbnvbn', 'Jean-Baptiste', 'acd54ba5-a244-4944-894f-82be2078cc05.jpg'),
        ('aoiupoilkj', 'Jean-Baptiste', 'acd54ba5-a244-4944-894f-82be2078cc05.jpg'),
        ('wxcwxcwxc', 'Jean-Baptiste', 'acd54ba5-a244-4944-894f-82be2078cc05.jpg')
-
 ;
 
 -- Insertion dans la table Categorie
@@ -91,60 +90,41 @@ VALUES ('Dupont', 'Jean', 'jean.dupont@example.com', '10 Rue de Paris', 'Paris',
        ('Blanc', 'Thomas', 'thomas.blanc@example.com', '50 Rue du Bac', 'Strasbourg', '67000', 10);
 
 -- Insertion dans la table Livre
-INSERT INTO Livre (liv_titre, liv_resume, liv_photo, liv_en_avant, aut_id, cat_id)
-VALUES ('Les Misérables', 'Un roman historique...', '1ad2a325-1021-4b83-bebb-db1932517e88.jpg', true, 1, 1),
-       ('Le Comte de Monte-Cristo', 'Un classique...', '2c196cd4-bd08-4bda-ae39-4f716f9419ea.jpg', false, 2, 1),
-       ('Germinal', 'Une fresque sociale...', '3ccdd34d-be42-4b21-b966-b5d925aa5d39.jpg', true, 3, 1),
-       ('L’Étranger', 'Un roman philosophique...', '3d1c8775-e9d1-4533-a385-92723d319713.jpg', false, 4, 4),
-       ('À la recherche du temps perdu', 'Un chef-d’œuvre...', '6e5ca510-2f7d-4693-bce2-75cafebe4887.jpg', false, 5, 1),
-       ('Les Mouches', 'Une pièce de théâtre...', '7b26596d-cf18-4f96-a12f-5871025820ae.jpg', true, 6, 3),
-       ('Vingt mille lieues sous les mers', 'Un roman d’aventure...', '55aae132-faf4-42e1-9c68-95f7a9cf4c63.jpg', true, 7, 2),
-       ('Madame Bovary', 'Une œuvre réaliste...', '61d56c41-05f3-4e61-8241-e3a8c331af6c.jpg', false, 8, 1),
-       ('La Comédie humaine', 'Un cycle de romans...', '283ae4b4-776a-48ab-8f61-6d17e018ed37.jpg', false, 9, 1),
-       ('Le Malade imaginaire', 'Une comédie...', '83479cc1-1cd5-4b7b-aa56-5ba8b109136f.jpg', true, 10, 3),
-       ('La gdfgdfge', 'Un cycle de romans...', '98704c2d-6347-468e-b662-25d27a82e67e.jpg', false, 9, 1),
-       ('La za	z	az', 'Un cycle de romans...', '03479322-0fae-42d9-9de1-3aef1883d3fa.jpg', false, 9, 1),
-       ('La piopio', 'Un cycle de romans...', 'af756329-11c4-4ca6-bfa3-fe4c52910474.jpg', false, 9, 1),
-       ('La aze', 'Un cycle de romans...', 'b3085602-6b68-4bfe-9f96-d0c508c8d094.jpg', false, 9, 1),
-       ('Lajhjghje', 'Un cycle de romans...', 'be798d86-d7bd-448d-b253-d5142ad30fe6.jpg', false, 9, 1),
-       ('qsdqsdqs', 'Undsfsdfsdfsd cycle fddsfsdf romans...', 'c43a3334-c7b6-4f4b-85e7-e7e5cee2dd5c.jpg', false, 9, 1),
-       ('erzerrhfg', 'Un sdfsdfscycle de romans...', 'd5c6e47d-0cbf-493c-882c-d00bd67be61d.jpg', false, 9, 1),
-       ('gfhgfhgmkl', 'Un cycle de romans...', 'dec11981-887d-4bc3-a600-8f63127754d7.jpg', false, 9, 1),
-       ('fdghfdhfd', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('pioupio', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('fddfhdfgh', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('mklmklm', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('azreze', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('nvbnvbn', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('mlkmklm', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('qsqsqs', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('piopuipio', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1),
-       ('zezezeze', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1)
+INSERT INTO Livre (liv_titre, liv_resume, liv_photo, liv_en_avant, aut_id, cat_id,quantite)
+VALUES ('Les Misérables', 'Un roman historique...', '1ad2a325-1021-4b83-bebb-db1932517e88.jpg', true, 1, 1,5),
+       ('Le Comte de Monte-Cristo', 'Un classique...', '2c196cd4-bd08-4bda-ae39-4f716f9419ea.jpg', false, 2, 1,4),
+       ('Germinal', 'Une fresque sociale...', '3ccdd34d-be42-4b21-b966-b5d925aa5d39.jpg', true, 3, 1,3),
+       ('L’Étranger', 'Un roman philosophique...', '3d1c8775-e9d1-4533-a385-92723d319713.jpg', false, 4, 4,2),
+       ('À la recherche du temps perdu', 'Un chef-d’œuvre...', '6e5ca510-2f7d-4693-bce2-75cafebe4887.jpg', false, 5, 1,5),
+       ('Les Mouches', 'Une pièce de théâtre...', '7b26596d-cf18-4f96-a12f-5871025820ae.jpg', true, 6, 3,4),
+       ('Vingt mille lieues sous les mers', 'Un roman d’aventure...', '55aae132-faf4-42e1-9c68-95f7a9cf4c63.jpg', true, 7, 2,8),
+       ('Madame Bovary', 'Une œuvre réaliste...', '61d56c41-05f3-4e61-8241-e3a8c331af6c.jpg', false, 8, 1,10),
+       ('La Comédie humaine', 'Un cycle de romans...', '283ae4b4-776a-48ab-8f61-6d17e018ed37.jpg', false, 9, 1,10),
+       ('Le Malade imaginaire', 'Une comédie...', '83479cc1-1cd5-4b7b-aa56-5ba8b109136f.jpg', true, 10, 3,4),
+       ('La gdfgdfge', 'Un cycle de romans...', '98704c2d-6347-468e-b662-25d27a82e67e.jpg', false, 9, 1,0),
+       ('La za	z	az', 'Un cycle de romans...', '03479322-0fae-42d9-9de1-3aef1883d3fa.jpg', false, 9, 1,0),
+       ('La piopio', 'Un cycle de romans...', 'af756329-11c4-4ca6-bfa3-fe4c52910474.jpg', false, 9, 1,0),
+       ('La aze', 'Un cycle de romans...', 'b3085602-6b68-4bfe-9f96-d0c508c8d094.jpg', false, 9, 1,0),
+       ('Lajhjghje', 'Un cycle de romans...', 'be798d86-d7bd-448d-b253-d5142ad30fe6.jpg', false, 9, 1,0),
+       ('qsdqsdqs', 'Undsfsdfsdfsd cycle fddsfsdf romans...', 'c43a3334-c7b6-4f4b-85e7-e7e5cee2dd5c.jpg', false, 9, 1,0),
+       ('erzerrhfg', 'Un sdfsdfscycle de romans...', 'd5c6e47d-0cbf-493c-882c-d00bd67be61d.jpg', false, 9, 1,0),
+       ('gfhgfhgmkl', 'Un cycle de romans...', 'dec11981-887d-4bc3-a600-8f63127754d7.jpg', false, 9, 1,0),
+       ('fdghfdhfd', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('pioupio', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('fddfhdfgh', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('mklmklm', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('azreze', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('nvbnvbn', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('mlkmklm', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('qsqsqs', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('piopuipio', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0),
+       ('zezezeze', 'Un cycle de romans...', 'ed062afb-9c40-44ab-b977-9f9e84c744bb.jpg', false, 9, 1,0)
 ;
 
--- Insertion dans la table Exemplaire
-INSERT INTO Exemplaire (liv_id)
-VALUES (1),
-       (1),
-       (2),
-       (3),
-       (4),
-       (5),
-       (6),
-       (7),
-       (8),
-       (9);
-
--- Insertion dans la table Reservation
-INSERT INTO Reservation (res_date, liv_id, cli_id)
-VALUES ('2025-01-10 10:00:00', 1, 1),
-       ('2025-01-11 12:00:00', 2, 2),
-       ('2025-01-12 15:00:00', 3, 3),
-       ('2025-01-13 14:30:00', 4, 4);
 
 -- Insertion dans la table Emprunter
-INSERT INTO Emprunter (res_id, emp_date_emprunt, emp_date_retour, cli_id, lib_id, exe_id)
-VALUES (1, '2025-01-10 11:00:00', '2025-01-20 11:00:00', 1, 1, 1),
-       (2, '2025-01-11 13:00:00', '2025-01-21 13:00:00', 2, 2, 2),
-       (3, '2025-01-12 16:00:00', NULL, 3, 1, 3),
-       (4, '2025-01-13 15:30:00', '2025-01-23 15:30:00', 4, 2, 4);
+INSERT INTO Emprunter ( emp_res_date,emp_date_emprunt, emp_date_retour, cli_id, lib_id, liv_id)
+VALUES ( '2025-01-09 11:00:00',  '2025-01-10 11:00:00', '2025-01-20 11:00:00', 1, 1, 1),
+       ('2025-01-10 13:00:00', '2025-01-11 13:00:00', '2025-01-21 13:00:00', 2, 2, 2),
+       ( '2025-01-11 16:00:00', '2025-01-12 16:00:00', NULL, 3, 1, 3),
+       ( '2025-01-12 15:30:00','2025-01-13 15:30:00', '2025-01-23 15:30:00', 4, 2, 4);
